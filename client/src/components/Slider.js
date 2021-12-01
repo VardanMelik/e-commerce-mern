966a4cc9a27e3928f7cd911389d95839cfa41ff3
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-02 00:33:27 +0400

    slider

--- client/src/components/Slider.js
+++ client/src/components/Slider.js
@@ -1,17 +1,40 @@
+import { useState } from 'react'
 import styled from 'styled-components';
 import ArrowRightOutlinedIcon from '@mui/icons-material/ArrowRightOutlined';
 import ArrowLeftOutlinedIcon from '@mui/icons-material/ArrowLeftOutlined';
+import { sliderItems } from './SliderData';
 
 function Slider() {
+    const [slideIndex, setSlideIndex] = useState(0);
+    const handleClick = (direction) => {
+        if (direction === "left") {
+            setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2 )
+        } else {
+            setSlideIndex(slideIndex < 2 ? slideIndex+1 : 0 )
+        }
+    }
+
     return (
         <Container>
-            <Arrow direction="left">
+            <Arrow direction="left" onClick={ () => handleClick("left")}>
                 <ArrowLeftOutlinedIcon/>
             </Arrow>
-            <Wrapper>
-
+            <Wrapper slideIndex={slideIndex}>
+                { sliderItems.map( item => (
+                    <Slide bg={item.bg}>
+                    <ImageContainer>
+                        <Image src={item.img}/>
+                    </ImageContainer>
+                    <InfoContainer>
+                        <Title>{item.title}</Title>
+                        <Description>{item.description}</Description>
+                        <Button>{item.buttonText}</Button>
+                    </InfoContainer>
+                </Slide>     
+                ))}
+                
             </Wrapper>    
-            <Arrow direction="right">
+            <Arrow direction="right" onClick={ () => handleClick("right")}>
                 <ArrowRightOutlinedIcon/>
             </Arrow>
         </Container>
@@ -24,6 +47,8 @@ const Container = styled.div`
     height: 100vh;
     display: flex;
     position: relative;
+    overflow: hidden;
+    //background-color: red;
 `
 
 const Arrow = styled.div`
@@ -42,12 +67,52 @@ const Arrow = styled.div`
     margin: auto;
     cursor: pointer;
     opacity: 0.5;
+    z-index: 2;
 `
 
 const Wrapper = styled.div`
     height: 100%;
+    display: flex;
+    transition: all 1.5s ease;
+    transform: translateX(${props => props.slideIndex * -100}vw);
 `
 
 const Slide = styled.div`
-
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    background-color: #${props => props.bg};
 `
+const ImageContainer = styled.div`
+    flex: 1;
+    height: 100%;
+`
+const Image = styled.img`
+    height: 80%;
+`
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+`
+
+const Title = styled.h1`
+    font-size: 70px;
+`
+
+const Description = styled.p`
+    margin: 50px 0px;
+    font-size: 20px;
+    font-weight: 500;
+    letter-spacing: 3px;
+`
+
+const Button = styled.button`
+    padding: 10px;
+    font-size: 20px;
+    background-color: transparent;
+    cursor: pointer;
+`
+
+
---
+++ client/src/components/SliderData.js
@@ -0,0 +1,44 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://img.shopperboard.com/22216061/29782676930715-1.jpg",
+        title: "SUMMER SALE",
+        description: "Don't compromise on style get flat 30% off for new arrivals.",
+        buttonText: "Show now",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://img.shopperboard.com/22216061/29782677029019-3.jpg",
+        title: "Winter SALE",
+        description: "Don't compromise on style get flat 30% off for new arrivals.",
+        buttonText: "Buy now",
+        bg: "fbf0f4"
+    },
+    {
+        id: 3,
+        img: "https://img.shopperboard.com/22216061/29782676996251-4.jpg",
+        title: "Autumn SALE",
+        description: "Don't compromise on style get flat 30% off for new arrivals.",
+        buttonText: "Try now",
+        bg: "fbfff4"
+    }
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://img.shopperboard.com/22216061/29782676930715-1.jpg",
+        title: "SUMMER SALE"
+    },
+    {
+        id: 2,
+        img: "https://img.shopperboard.com/22216061/29782677029019-3.jpg",
+        title: "Winter SALE",
+    },
+    {
+        id: 3,
+        img: "https://img.shopperboard.com/22216061/29782676996251-4.jpg",
+        title: "Autumn SALE",
+    }
+]
