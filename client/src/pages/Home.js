b1582f495cc2c75bd631cd51883236144899cc3d
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-01 23:34:38 +0400

    update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import Announcement from '../components/Announcement'
 import Nabvar from '../components/Nabvar'
+import Slide  from '../components/Slider'
 
 function Home() {
     return (
         <div>
             <div className="container">
+                <Announcement />
                 <Nabvar />
+                <Slide />
             </div>
         </div>
     )
