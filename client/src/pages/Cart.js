3ece1f9e38a78bd46639bfde7b2a68d452ab28f4
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-04 00:19:01 +0400

    update

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Nabvar from '../components/Nabvar'
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
 
 function Cart() {
     return (
@@ -27,15 +29,71 @@ function Cart() {
                                 <Details>
                                     <ProductName><b>Product: </b>It's looks very nice</ProductName>
                                     <ProductId><b>Product ID: </b> 1345625789</ProductId>
-                                    <ProductColor/>
+                                    <ProductColor color="black"/>
                                     <ProductSize><b>Size: </b> 37.5</ProductSize>
                                 </Details>
                             </ProductDetail>
-                            <PriceDetail>Price</PriceDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <AddIcon/>
+                                    <ProductAmount>
+                                        2
+                                    </ProductAmount>
+                                    <RemoveIcon/>
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 3</ProductPrice>
+                            </PriceDetail>
+                            <PriceDetail></PriceDetail>
+                        </Product>
+                        
+                        <Hr/>
+
+                        <Product>
+                            <ProductDetail>
+                                <Image src="http://www.wholesaleclothesturkey.com/women-clothes-wholesale-istanbul%20(1).jpg" />
+                                <Details>
+                                    <ProductName><b>Product: </b>It's red dress</ProductName>
+                                    <ProductId><b>Product ID: </b> 1345625778</ProductId>
+                                    <ProductColor color="gray"/>
+                                    <ProductSize><b>Size: </b> 27.5</ProductSize>
+                                </Details>
+                            </ProductDetail>
+                            <PriceDetail>
+                                <ProductAmountContainer>
+                                    <AddIcon/>
+                                    <ProductAmount>
+                                        3
+                                    </ProductAmount>
+                                    <RemoveIcon/>
+                                </ProductAmountContainer>
+                                <ProductPrice>$ 68</ProductPrice>
+                            </PriceDetail>
                             <PriceDetail></PriceDetail>
                         </Product>
                     </Info>
-                    <Summary>Summary</Summary>
+                    <Summary>
+                        <SummaryTitle>Order summary</SummaryTitle>
+                        <SummaryItem>
+                            <SummaryItemText>Subtotal</SummaryItemText>
+                            <SummaryItemPrice>$ 80</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <SummaryItem>
+                            <SummaryItemText>Estimated shipping</SummaryItemText>
+                            <SummaryItemPrice>$ 5.60</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <SummaryItem>
+                            <SummaryItemText>Shipping discount</SummaryItemText>
+                            <SummaryItemPrice>$ -5.90</SummaryItemPrice>
+                        </SummaryItem>
+
+                        <SummaryItem type="total">
+                            <SummaryItemText>Total</SummaryItemText>
+                            <SummaryItemPrice>$ 80</SummaryItemPrice>
+                        </SummaryItem>
+                        <Button>Checkout now</Button>
+                    </Summary>
                 </Bottom>
             </Wrapper>
             <Footer/>
@@ -91,23 +149,99 @@ const Info = styled.div`
     flex: 3;
 `
 
-const Summary = styled.div`
-    flex: 1;
+const Product = styled.div`
+    display: flex;
+    justify-content: space-between;
 `
-const Product = styled.div``
     
-const ProductDetail = styled.div``
+const ProductDetail = styled.div`
+    flex: 2;
+    display: flex;
+`
 
-const PriceDetail = styled.div``
+const PriceDetail = styled.div`
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
 
-const Image = styled.img``
+const Image = styled.img`
+    width: 200px;
+`
 
-const Details = styled.div``
+const Details = styled.div`
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+`
 
 const ProductName = styled.span``
 
 const ProductId = styled.span``
 
-const ProductColor = styled.div``
+const ProductColor = styled.div`
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    background-color: ${props => props.color};
+`
 
 const ProductSize = styled.span``
+
+const ProductAmountContainer = styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+`
+
+const ProductAmount = styled.div`
+    font-size: 24px;
+    margin: 5px;
+`
+
+const ProductPrice = styled.div`
+    font-size: 30px;
+    font-weight: 200;
+`
+
+const Hr = styled.hr`
+    background-color: #eee;
+    border: none;
+    height: 1px;
+    margin: 10px 0px;
+`
+const Summary = styled.div`
+    flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
+`
+
+
+const SummaryTitle = styled.h1`
+    font-weight: 200;
+`
+
+const SummaryItem = styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props => props.type == "total" && "500"};
+    font-size: ${props => props.type == "total" && "24px"};
+`
+
+const SummaryItemText =styled.span``
+
+const SummaryItemPrice = styled.span``
+
+const Button = styled.button`
+    width: 100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+`
