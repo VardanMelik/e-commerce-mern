3fe1701e24a09d5850b55bd063f48bc04648dc44
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-06 19:28:19 +0400

    users

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
-const { verifyToken } = require('./verifyToken')
+const User = require('../models/User');
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
 
-router.put('/:id', verifyToken,  async (req, res) => {
+router.put('/:id', async (req, res, next) => {
+    if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString();
+    }
 
-    if (req.user.id === req.params.id || req.user.isAdmin) {
-        
-    } 
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+            $set: req.body
+        }, { new: true });
 
+        res.status(201).json({
+            'username': updatedUser.username,
+            'email': updatedUser.email,
+            'isAdmin': updatedUser.isAdmin
+        });
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+    
 })
 
 module.exports = router
--- server/routes/verifyToken.js
+++ server/routes/verifyToken.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken')
 
 const verifyToken = (req, res, next) => {
-
     const authHeader = req.headers.token;
     if (authHeader) {
+        const token = authHeader.split(" ")[1];
         jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
             if (error) {
                 return res
@@ -22,8 +22,18 @@ const verifyToken = (req, res, next) => {
     }
 }
 
-const verifyTokenAndAutherization = () => {
-    
+const verifyTokenAndAuthorization = (req, res, next) => {
+
+    verifyToken(req, res, () => {
+        if (req.user.id === req.params.id || req.user.isAdmin) {
+            next();
+        } else {
+            res
+            .status(403)
+            .json('You have not access to that part')
+        }
+    })
 }
 
-module.export = { verifyToken }
+
+module.export = { verifyToken, verifyTokenAndAuthorization }
