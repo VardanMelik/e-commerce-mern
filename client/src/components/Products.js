513788a52c7fe3fecd648c79733944411bfdbba2
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-12-11 10:52:47 +0400

    update

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -1,14 +1,43 @@
+import { useState, useEffect } from 'react' 
 import styled from 'styled-components'
 import Product from './Product'
 import { categories, popularProducts } from './SliderData'
+import axios from 'axios'
 
-function Products({ category, filter, sort }) {
-    console.log(category, filter, sort)
+function Products({ category, filters, sort }) {
+    const [products, setProducts] = useState([]);
+    const [filteredProducts, setFilterdProducts] = useState([])
+
+    useEffect( () => {
+        const getProducts = async () => {
+            try {
+                const response = await axios.post( category 
+                    ? `http://localhost:5000/api/products/findall?category=${category}` 
+                    : `http://localhost:5000/api/products/findall`);
+                setProducts(response.data);
+                console.log(products);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        getProducts();
+    }, [category]);
+
+    useEffect(() => {
+        category &&
+        setFilterdProducts(
+            products.filter((item) =>
+              Object.entries(filters).every(([key, value]) =>
+                item[key].includes(value)
+              )
+            )
+          );
+      }, [products, category, filters]);
 
     
     return (
         <Container>
-            {popularProducts.map( item => (
+            {filteredProducts.map( item => (
                 <Product item={item} key={item.id}/>
             ))}
         </Container>
